03820df19ee5bbaf13de86a235a4eb5159e79c89
lhw421 <gpdnjs421@gmail.com>
2022-04-05 01:00:47 +0900

    시맨틱하게 수정

--- 001_Bin2Dec/001_script.js
+++ 001_Bin2Dec/001_script.js
@@ -1,22 +1,23 @@
-var bin = document.getElementById("bin");
-var dec = document.getElementById("dec");
-var convertButton = document.getElementById("convert");
-var errMessageSection = document.getElementById("error");
-errMessageSection.style.visibility = "hidden";
+let bin = document.getElementById("binInput");
+let dec = document.getElementById("decInput");
+const convertButton = document.getElementById("convertButton");
+const errorMessageSection = document.getElementById("errorMessage");
+errorMessageSection.style.visibility = "hidden";
 
 convertButton.onclick = decConvert;
 
 function decConvert(){
-    var digits = bin.value;
+    let digits = bin.value;
     
-    for(var digit of digits){
+    for(let digit of digits){
         // console.log(digit);
         if(!(digit in ["0", "1"])){
-            errMessageSection.style.visibility = "visible";
+            errorMessageSection.style.visibility = "visible";
             dec.value = null;
             return;
         }
     }
+    
     dec.value = parseInt(digits, 2);
-    errMessageSection.style.visibility = "hidden";
+    errorMessageSection.style.visibility = "hidden";
 }
